009bde7d705f2efdded7ff0469b9b4d1abff6f72
standy <mostovoyav@gmail.com>
2014-11-28 01:40:06 +0300

    deploy upd

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ var gulp = require('gulp');
 var ghpages = require('gulp-gh-pages');
 
 
+gulp.task('prepare', function () {
+	return gulp.src(['data-json/**/*', 'static/**/*', 'tpl/**/*', 'index.html'], {base: './'})
+		.pipe(gulp.dest('gh-pages'));
+});
+gulp.task('deploy2', ['prepare'], function () {
+	return gulp.src('gh-pages/**/*')
+		.pipe(ghpages());
+});
 gulp.task('deploy', function () {
-	return gulp.src(['**/*', '!node_modules', '!node_modules/**/*', '!data-json-raw', '!data-json-raw/**/*', '!gulpfile.js', '!get-data.js', '!package.json'])
+	return gulp.src(['data-json/**/*', 'static/**/*', 'tpl/**/*', 'index.html'], {base: './', maxBuffer: 64 * 1024 * 1024})
 		.pipe(ghpages());
 });
